1a386c4c7466232d82539e981cd3530f00740118
SillySanjay <sanjaysoni29032003@gmail.com>
2025-12-05 15:37:37 +0530

    updated url for cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 // app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://a-i-code-reviewer-frontend.onrender.com",
     
 }))
 
